0ee1d9dfdc9d050fbbba587e4fefcd79ddc6d84e
BeileiTan <beilei0508@gmail.com>
2025-02-02 21:11:59 -0800

    This is the first commit.

---
+++ src/main/java/com/springdemo/receiptprocessoronline/ReceiptProcessorOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.receiptprocessoronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptProcessorOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptProcessorOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdemo/receiptprocessoronline/ReceiptProcessorOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.receiptprocessoronline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptProcessorOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
